3b5653ffedd84fbbc8a5eaa8e2409026a7a83858
unknown <mjberhe123@hotmail.com>
2021-05-14 01:21:28 -0400

    test

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,40 @@ import Head from 'next/head';
 import Link from 'next/link';
 
 export default function MyApp() {
+
+   const handleButton = (e) => {
+      e.preventDefault();
+      location.href = "/gamelobby";
+   }
+
    return (
       <div className="landing-page-container">
          <Head>
             <title>Lobby</title>
             <link rel="icon" type="image/png" href="/omega.png" />
          </Head>
-         <div>
-            <h1>
-               Join the <Link href="/gamelobby">
-                  <a className='link-text'>Game Lobby</a>
-               </Link>
-            </h1>
-            {/* <h1>
-               Join the <Link href="/chatlobby"><a>chat lobby</a></Link>
-            </h1> */}
+
+         <div className='landing-page-content'>
+            <nav>
+               <h3>2S</h3>
+               <h3>Create Account</h3>
+            </nav>
+            <div className='quote-button'>
+               <div className='quote'>
+                  <h1>Insert motivational yet provacative quote.</h1>
+               </div>
+               <button className='button' onClick={handleButton}>
+                  <h2>Play Now</h2>
+               </button>
+            </div>
+            
          </div>
       </div>
    )
 }
+
+
+{/* <Link href="/gamelobby">
+<a className='link-text'>Play Now</a>
+</Link>
+</button> */}
